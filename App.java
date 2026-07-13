64f29d17fad5abf422549c287401fb841dc0309e
Mel Kengne <foyetmel237@gmail.com>
2026-07-13 22:45:20 +0200

    Update App.java

--- App.java
+++ App.java
@@ -1,1 +1,5 @@
-public class App { public static void main(String[] args) System.out.println("Bravo, le Webhook Jenkins fonctionne à merveille !"); } }
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bravo, le Webhook Jenkins fonctionne a merveille !");
+    }
+}
